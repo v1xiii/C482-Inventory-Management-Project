05c59034819583a4bbd18e582b4f7b0c6e1afd0d
Lee Scholl <v1xiii@gmail.com>
2019-05-20 21:27:42 -0500

    Added a new window and played with some stage and scene options.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,35 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
 
-public class Main extends Application {
+public class Main extends Application { //main calls launch, which calls start
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.setTitle("Inventory Management System"); // stage title
+        primaryStage.setScene(new Scene(root, 600, 550)); // scene to be displayed on primaryStage
+        primaryStage.show(); // shows this stage
+
+        VBox addPartVBox = new VBox(new Label("Add Part")); // what is VBox?
+        Scene addPartScene = new Scene(addPartVBox, 300, 275); // make another scene, pass VBox to it
+
+        Stage addPartStage = new Stage(); // make another stage
+        addPartStage.setTitle("Add Part"); // set title
+        //addPartStage.setCursor(Cursor.OPEN_HAND); // set cursor
+        //addPartStage.initStyle(StageStyle.UNDECORATED); // set window decoration
+        addPartStage.initOwner(primaryStage); // make primaryStage own this stage, but why???
+        addPartStage.setScene(addPartScene); // set scene to stage
+        addPartStage.initModality(Modality.APPLICATION_MODAL); // make this stage a blocking modal
+        addPartStage.show(); // show it!
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args); // launches the JavaFX application (calls start)
+
     }
 }
