5e3243d40253e76f873c919fe63f43969d8d5e92
Lee Scholl <v1xiii@gmail.com>
2019-05-28 22:10:45 -0500

    Added XML for add part screen. Made exit button work.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,23 +16,23 @@ public class Main extends Application { //main calls launch, which calls start
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main-screen.fxml"));
         primaryStage.setTitle("Inventory Management System"); // stage title
-        Button exit = new Button("Exit Program"); // new button
-        exit.setWrapText(true); // button text wrap
-        exit.setMaxSize(200, 100); // button size
+        //Button exit = new Button("Exit Program"); // new button
+        //exit.setWrapText(true); // button text wrap
+        //exit.setMaxSize(200, 100); // button size
         //primaryStage.setScene(new Scene(new StackPane(exit), 600, 550)); // scene is sent a stackpane, stackpane is sent a button
         primaryStage.setScene(new Scene(root, 1500, 750));
         primaryStage.show(); // shows this stage
 
-        VBox addPartVBox = new VBox(new Label("Add Part")); // VBox positions child nodes in vertical row
-        Scene addPartScene = new Scene(addPartVBox, 300, 275); // make another scene, pass VBox vertical row to it
+        //VBox addPartVBox = new VBox(new Label("Add Part")); // VBox positions child nodes in vertical row
+        //Scene addPartScene = new Scene(addPartVBox, 300, 275); // make another scene, pass VBox vertical row to it
 
-        Stage addPartStage = new Stage(); // make another stage
-        addPartStage.setTitle("Add Part"); // set title
+        //Stage addPartStage = new Stage(); // make another stage
+        //addPartStage.setTitle("Add Part"); // set title
         //addPartStage.setCursor(Cursor.OPEN_HAND); // set cursor
         //addPartStage.initStyle(StageStyle.UNDECORATED); // set window decoration
-        addPartStage.initOwner(primaryStage); // make primaryStage own this stage, but why???
-        addPartStage.setScene(addPartScene); // set scene to stage
-        addPartStage.initModality(Modality.APPLICATION_MODAL); // make this stage a blocking modal
+        //addPartStage.initOwner(primaryStage); // make primaryStage own this stage, but why???
+        //addPartStage.setScene(addPartScene); // set scene to stage
+        //addPartStage.initModality(Modality.APPLICATION_MODAL); // make this stage a blocking modal
         //addPartStage.show(); // show it!
     }
 
--- src/sample/MainScreenController.java
+++ src/sample/MainScreenController.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class MainScreenController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    public void exitProgram(ActionEvent event) {
+        System.exit(0);
+    }
 }
